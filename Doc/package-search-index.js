21fecd1bb45cb18f0e15c74cbc74fb0aea03dfce
bfox1 <robert.gamiz@gmail.com>
2021-07-22 14:08:34 -0700

    Added JavaDoc

---
+++ Doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.github.bfox1"},{"l":"io.github.bfox1.command"},{"l":"io.github.bfox1.embed"},{"l":"io.github.bfox1.event"},{"l":"io.github.bfox1.manager"},{"l":"io.github.bfox1.module"},{"l":"io.github.bfox1.user"},{"l":"io.github.bfox1.utils"}]
